e3c1f8a83dfbbaca646f4824cf4f1f10eade6421
IvanAnikin <13561+IvanAnikin@users.noreply.github.com>
2019-10-19 16:32:43 +0200

    added azure.js libraries added page.html for next html upload TODO: how to post somethin g to blob azure from html js

--- Azure-storage_1/Azure-storage_1/Program.cs
+++ Azure-storage_1/Azure-storage_1/Program.cs
@@ -19,6 +19,107 @@ namespace Azure_storage_1
             Console.ReadLine();
         }
 
+        private static async Task UploadPhotoFromPC()
+        {
+            CloudStorageAccount storageAccount = null;
+            CloudBlobContainer cloudBlobContainer = null;
+            string sourceFile = null;
+
+            Console.WriteLine("Enter storageConnectionString:");
+            string storageConnectionString = Console.ReadLine();
+            if (CloudStorageAccount.TryParse(storageConnectionString, out storageAccount))
+            {
+
+                try
+                {
+                    CloudBlobClient cloudBlobClient = storageAccount.CreateCloudBlobClient();
+
+                    Console.WriteLine("Do you want a new container? Y/N");
+                    string input = Console.ReadLine();
+
+                    if (input == "Y" || input == "y")
+                    {
+                        cloudBlobContainer = cloudBlobClient.GetContainerReference("quickstartblobs" + Guid.NewGuid().ToString());
+                        await cloudBlobContainer.CreateAsync();
+                        Console.WriteLine("Created container '{0}'", cloudBlobContainer.Name);
+                        Console.WriteLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter the name of the container.");
+                        cloudBlobContainer = cloudBlobClient.GetContainerReference(Console.ReadLine());
+                        Console.WriteLine("Got the container. {0}", cloudBlobContainer.Name);
+                    }
+
+                    string path = "C:\\Users\\ivana\\OneDrive\\Desktop\\";
+                    string file = "duckTest.jpg";
+
+                    path = ChangePath(path);
+                    file = ChangeFile(file);
+
+                    sourceFile = Path.Combine(path, file);
+
+                    Console.WriteLine("Temp file = {0}", sourceFile);
+
+                    Console.WriteLine("Do you want to upload the file? Y/N");
+                    input = Console.ReadLine();
+
+                    if (input == "Y" || input == "y")
+                    {
+                        Console.WriteLine("Uploading to Blob storage as blob '{0}'", file);
+                        Console.WriteLine();
+
+                        CloudBlockBlob cloudBlockBlob = cloudBlobContainer.GetBlockBlobReference(file);
+                        await cloudBlockBlob.UploadFromFileAsync(sourceFile);
+                    }
+
+                    Console.WriteLine("Listing blobs in container.");
+
+                    BlobContinuationToken blobContinuationToken = null;
+                    do
+                    {
+                        var results = await cloudBlobContainer.ListBlobsSegmentedAsync(null, blobContinuationToken);
+                        // Get the value of the continuation token returned by the listing call. 
+                        blobContinuationToken = results.ContinuationToken;
+
+                        foreach (IListBlobItem item in results.Results)
+                        {
+                            Console.WriteLine(item.Uri);
+                        }
+                    } while (blobContinuationToken != null); // Loop while the continuation token is not null.
+
+                    Console.WriteLine();
+                }
+
+
+                catch (StorageException ex)
+                {
+                    Console.WriteLine("Error returned from the service: {0}", ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("Do you want to delete the container? Y/N");
+                    string input = Console.ReadLine();
+                    if (input == "Y" || input == "y" && cloudBlobContainer != null)
+                    {
+                        Console.WriteLine("Deleting the container and any blobs it contains");
+                        await cloudBlobContainer.DeleteIfExistsAsync();
+                    }
+                }
+
+            }
+            else
+            {
+                Console.WriteLine(
+                    "A connection string has not been defined in the system environment variables. " +
+
+                    "Add a environment variable named 'storageconnectionstring' with your storage " +
+
+                    "connection string as a value.");
+            }
+
+        }
+
         private static async Task ProcessAsync()
         {
             CloudStorageAccount storageAccount = null;
